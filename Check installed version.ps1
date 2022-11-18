4005c605afd937591ab692fed74d28b1ee23c30d
Angel Garza <angelgnzlz0@gmail.com>
2022-11-18 10:17:37 -0600

    Test message

--- Check installed version.ps1
+++ Check installed version.ps1
@@ -1,2 +1,5 @@
 Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
 foreach($obj in $InstalledSoftware){write-host $obj.GetValue('DisplayName') -NoNewline; write-host " - " -NoNewline; write-host $obj.GetValue('DisplayVersion')}
+
+
+#this is a test
