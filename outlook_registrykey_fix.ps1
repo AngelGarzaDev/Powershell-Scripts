bc0398d36679a2aad0713f139ad46708a0816898
Angel Garza <angelgnzlz0@gmail.com>
2022-11-22 09:39:46 -0600

    Restored code

--- outlook_registrykey_fix.ps1
+++ outlook_registrykey_fix.ps1
@@ -1,3 +1,9 @@
 #Run as current user, not Administrator
 
-.
+New-Item -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity" -Name "DisableAADWAM"
+New-Item -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity" -Name "DisableADALatopWAMOverride"
+New-Item -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity" -Name "EnableADAL"
+
+New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity" -Name "DisableAADWAM" -Value 1 -PropertyType DWord
+New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity" -Name "DisableADALatopWAMOverride" -Value 1 -PropertyType DWord
+New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity" -Name "EnableADAL" -Value 1 -PropertyType DWord
