8d5736b1002e399d5452c3c80e629483a3842314
Angel Garza <angelgnzlz0@gmail.com>
2023-01-11 10:15:09 -0600

    enter

--- outlook_registrykey_fix.ps1
+++ outlook_registrykey_fix.ps1
@@ -7,3 +7,4 @@ New-Item -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity" -Name "Ena
 New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity" -Name "DisableAADWAM" -Value 1 -PropertyType DWord
 New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity" -Name "DisableADALatopWAMOverride" -Value 1 -PropertyType DWord
 New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity" -Name "EnableADAL" -Value 1 -PropertyType DWord
+
