7b15d1ffa6340b8d749291826f79c9a174ad9ffb
Angel Garza <angelgnzlz0@gmail.com>
2023-08-02 14:36:01 -0500

    Updated Script to target version 20.3.15 of Bluebeam Revu

--- add-registrykey-BB_Update.ps1
+++ add-registrykey-BB_Update.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference= 'silentlycontinue'
 
-New-ItemProperty -Path "HKCU:\Software\Bluebeam Software\20\Revu" -Name "InAppUpdateVersionToSkip" -Value 20.2.90 -PropertyType String
+New-ItemProperty -Path "HKCU:\Software\Bluebeam Software\20\Revu" -Name "InAppUpdateVersionToSkip" -Value 20.3.15 -PropertyType String
 
 Remove-ItemProperty -Path "HKCU:\Software\Bluebeam Software\20\Revu" -Name "InAppUpdatePostponed"
