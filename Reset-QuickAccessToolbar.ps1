26cf13115d81d67309858eb5f34067ace4a0b615
Angel Garza <angelgnzlz0@gmail.com>
2023-09-08 10:25:44 -0500

    ...

---
+++ Reset-QuickAccessToolbar.ps1
@@ -0,0 +1,4 @@
+# To reset the quick access toolbar to its defaults, deleted the files in the following two directories
+
+# shell:recent\AutomaticDestinations
+# shell:recent\CustomDestinations
--- send-email.ps1
+++ send-email.ps1
@@ -1,9 +1,9 @@
 Send-MailMessage `
     -To "angel.garza@spawglass.com" `
-    -From "han.solo@spawglass.com" `
+    -From "angel.garza.noreply@spawglass.com" `
     -Subject "This is a test message from PowerShell" `
     -Body "Hello World!" `
     -Credential (Get-Credential) `
-    -SmtpServer "smtp.office365.com" `
-    -Port 587 `
+    -SmtpServer 192.168.10.22 `
+    -Port 25 `
     -UseSsl    #If mail server not support SSL/TLS, let remove this parameter
